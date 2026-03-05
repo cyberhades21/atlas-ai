d9cd19bf4af9beb0ee6667ac2fe7a9d212b43e90
cyberhades21 <shyamsundar2101@gmail.com>
2026-03-05 21:51:57 +0530

    UI skeleton

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from app.api.documents import router as document_router
 from app.api.query import router as query_router
 
@@ -7,7 +10,9 @@ app = FastAPI(title="Atlas AI")
 app.include_router(document_router)
 app.include_router(query_router)
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 @app.get("/")
-def root():
-    return {"message": "Atlas AI Running"}
+def home():
+    return FileResponse("app/static/index.html")
